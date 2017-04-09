dbdfe67a903fa155168992ac724850fddd43560d
Robert Jan Zwetsloot <zwetsloot.robert.jan@techcross.co.jp>
2017-04-09 10:54:15 +0900

    initialize the project for mythcraft, a simple game intended to get familiar with the elixir language

---
+++ lib/mythcraft.ex
@@ -0,0 +1,18 @@
+defmodule Mythcraft do
+  @moduledoc """
+  Documentation for Mythcraft.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mythcraft.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mythcraft_test.exs
@@ -0,0 +1,8 @@
+defmodule MythcraftTest do
+  use ExUnit.Case
+  doctest Mythcraft
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
